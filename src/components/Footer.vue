5552337033a91d931f8a122a1478723381087bad
Admir <admir.ferhatovic@gmail.com>
2018-05-24 07:47:11 +0200

    Huge changes

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="footer">
+    <p class="footer-text">Footer inc.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+  .footer {
+    position: fixed;
+    width: 100%;
+    left: 0;
+    bottom: 0;
+    text-align: right;
+    background-color: rgba(66, 66, 66, 0.3);
+    color: whitesmoke;
+    box-shadow: 4px 0 4px black;
+    height: 50px;
+    z-index: 1;
+  }
+  .footer-text {
+    margin-right: 12px;
+  }
+</style>
---
+++ src/components/ListView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="list-view">
+    <div class="list-item" v-for="item in $store.state.items" :key="item.id">
+      <h3>{{ item.name }}</h3>
+      <el-row>
+        <el-col :span="8">
+          <img src="http://via.placeholder.com/140x100" style="width:80px;height:80px; text-align: left"/>
+        </el-col>
+        <el-col :span="16" style="text-align: left;">
+          <p>{{ item.description }}</p>
+          <label>{{ item.price }}$</label>
+        </el-col>
+      </el-row>
+      <el-button type="info" class="clearfix" style="float: right; margin-right: 12px">Order</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListView'
+}
+</script>
+
+<style scoped>
+  .list-view {
+    text-align: center;
+  }
+  .list-item {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    color: whitesmoke;
+    background-color: rgba(66, 66, 66, 0.3);
+    box-shadow: 0 0 4px black;
+    margin-left: 20%;
+    margin-right: 20%;
+    overflow: auto;
+  }
+  .clearfix {
+    overflow: auto;
+  }
+</style>
